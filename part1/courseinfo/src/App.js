5ef159d5538aed6a1a41944555aa9648c8e4ce3c
Alejo Conde <alejo.conde.97@gmail.com>
2023-09-28 22:49:33 -0300

    part 1: exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -18,16 +18,18 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
+        <Part part={part1} exercise={exercises1} />
+        <Part part={part2} exercise={exercises2} />
+        <Part part={part3} exercise={exercises3} />
       </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercise}
+      </p>
     )
   }
 
