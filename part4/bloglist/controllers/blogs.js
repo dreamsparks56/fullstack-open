e00fcd9253cda8b58521c1163d4e80b8da1fc94a
Alejo Conde <alejo.conde.97@gmail.com>
2023-12-17 18:55:00 -0300

    part4: exercise 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const Blog = require('../models/blog')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username:1, name: 1 })
@@ -28,11 +20,15 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
   const user = await User.findById(decodedToken.id)
+  if (!decodedToken.id) {
+    return response.status(401).json({
+      error: 'token invalid'
+    })
+  }
 
   const blog = new Blog({
     author: body.author,
