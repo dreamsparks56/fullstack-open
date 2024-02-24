1eeff5e354b6def0225ea4153dc589e274a22087
Alejo Conde <alejo.conde.97@gmail.com>
2024-02-24 15:00:27 -0300

    part5: exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog', () => {
-  test('renders content right', () => {
-    const blog = {
-      title: 'New title',
-      author: 'New author',
-      url: 'www.url.com',
-      likes: 0
+  let container
+  const blog = {
+    title: 'New title',
+    author: 'New author',
+    url: 'www.url.com',
+    likes: 0,
+    user: {
+      name: 'usw3rn4m3'
     }
+  }
 
-    const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(<Blog blog={blog} verifyId={() => false}/>).container
+  })
 
+  test('renders content right initially', () => {
     const div = container.querySelector('.blog')
+
     expect(div).toHaveTextContent(
       'New title'
     )
@@ -25,6 +33,22 @@ describe('Blog', () => {
       'www.url.com'
     )
     expect(div).not.toHaveTextContent(
+      '0'
+    )
+  })
+
+  test('renders content right after expanding', async () => {
+
+    const div = container.querySelector('.blog')
+    const toggler = screen.getByText('expand')
+
+    await userEvent.click(toggler)
+    screen.debug()
+
+    expect(div).toHaveTextContent(
+      'www.url.com'
+    )
+    expect(div).toHaveTextContent(
       '0'
     )
   })
