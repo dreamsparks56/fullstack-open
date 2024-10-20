4db620e8088503671e37cb6a5b1cdcb1ef9dfcee
Alejo Conde <alejo.conde.97@gmail.com>
2024-10-20 10:07:24 -0300

    part7: exercise 7.1

---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import CreateNew from './components/CreateNew'
+import About from './components/About'
+import AnecdoteList from './components/AnecdoteList'
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (    
+  <div>
+    <Link style={padding} to="/">anecdotes</Link>
+    <Link style={padding} to="/create">create new</Link>
+    <Link style={padding} to="/about">about</Link>
+  </div>
+  )
+}
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+
+    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+  </div>
+)
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  const [notification, setNotification] = useState('')
+
+  const addNew = (anecdote) => {
+    anecdote.id = Math.round(Math.random() * 10000)
+    setAnecdotes(anecdotes.concat(anecdote))
+  }
+
+  const anecdoteById = (id) =>
+    anecdotes.find(a => a.id === id)
+
+  const vote = (id) => {
+    const anecdote = anecdoteById(id)
+
+    const voted = {
+      ...anecdote,
+      votes: anecdote.votes + 1
+    }
+
+    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  }
+
+  return (
+    <Router>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} /> }/>
+        <Route path="/about" element={<About /> } />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
